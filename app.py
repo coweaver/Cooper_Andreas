03b63468244247a692e98bea33422841532f2d2a
coweaver <cooperweaver@Macintosh-0019e3454091-2.local>
2014-10-30 14:07:14 -0400

    Removed utils folder, put dbmanager next to app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
-from utils import dbmanager
+from dbmanager import *
 
 
 app=Flask(__name__)
 
+@app.route("/", methods=["GET","POST"])
 @app.route("/login", methods=["GET","POST"])
 def login():
     if request.method=="GET":
@@ -11,12 +12,13 @@ def login():
     else:
         email = request.form["usermail"]
         pword = request.form["pword"]
-        valid = dbmanager.authenticate(email,pword)
+        valid = authenticate(email,pword)
         if not(valid):
             return render_template("login.html")
         else:
-            name = dbmanager.getName(email)
+            name = getName(email)
             return render_template(Home.html,name=name)
+
 
 @app.route("/register", methods=["GET","POST"])
 def register():
@@ -26,7 +28,13 @@ def register():
         email = request.form["usermail"]
         pword = request.form["pword"]
         name = request.form["name"]
-        valid_email = dbmanager.checkEmail(email)
+        valid_email = checkEmail(email)
         if not(valid_email):
             error = "Email Already Exists"
             return render_template("register.html", error=error)
+
+
+
+if __name__=="__main__":
+    app.debug=True
+    app.run()
