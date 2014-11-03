ba5e6143354290a36ed59fe07ce475f28080f8be
Andreas Wang <awang289@gmail.com>
2014-11-03 12:31:34 -0500

    session stuff

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from dbmanager import *
 
 
@@ -7,6 +7,8 @@ app=Flask(__name__)
 @app.route("/", methods=["GET","POST"])
 @app.route("/login", methods=["GET","POST"])
 def login():
+    if  'n' not in session:
+        session['n'] = None
     print_Everything()
     if request.method=="GET":
         return render_template("login.html")
@@ -18,7 +20,8 @@ def login():
             return render_template("login.html", error="Not a valid Email or Password")
         else:
             name = getName(email)
-            return render_template("home.html",name=name)
+            session['n'] = name
+            return render_template("secure_page.html",name=name)
 
 
 @app.route("/register", methods=["GET","POST"])
@@ -34,10 +37,22 @@ def register():
             error = "Email Already Exists"
             return render_template("register.html", error=error)
         add(name,email,pword)
-        return redirect(url_for("/login"))
+        return redirect(url_for('login'))
 
+    
+@app.route("/secure_page", methods = ["GET", "POST"])
+def secure_page():
+    if 'n' not in session:
+        session['n'] = None
+    return render_template("secure_page.html", name = session['n'])
+
+@app.route("/logout")
+def logout():
+    session.pop('n',None)
+    return redirect("/")
 
 
 if __name__=="__main__":
+    app.secret_key="This is a secret key"
     app.debug=True
     app.run()
